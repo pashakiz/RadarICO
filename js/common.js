481a5373d0a02f99d93fe0079725acbf50704d66
pashakiz <pashakiz@gmail.com>
2017-08-27 20:01:48 +0300

    header finished, footer finished, first-screen almost finished

--- js/common.js
+++ js/common.js
@@ -4,4 +4,15 @@ $(document).ready(function() {
 	//Doc: https://github.com/NV/placeholder.js/
 	$("[placeholder]").textPlaceholder();
 
+    //Кнопка "Наверх"
+    //Документация:
+    //http://api.jquery.com/scrolltop/
+    //http://api.jquery.com/animate/
+    $("#top").on('click', function () {
+        $("body, html").animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
 });
