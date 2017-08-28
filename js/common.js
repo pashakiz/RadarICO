368bca12238e23903c329c3f1e66363bf4ce9828
pashakiz <pashakiz@gmail.com>
2017-08-28 21:10:59 +0300

    mobile version (main page)

--- js/common.js
+++ js/common.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
 
-	//Placeholder
-	//Doc: https://github.com/NV/placeholder.js/
+	// Placeholder
+	// Doc: https://github.com/NV/placeholder.js/
 	$("[placeholder]").textPlaceholder();
 
-    //Кнопка "Наверх"
-    //Документация:
-    //http://api.jquery.com/scrolltop/
-    //http://api.jquery.com/animate/
+    // UP button
     $("#top").on('click', function () {
         $("body, html").animate({
             scrollTop: 0
         }, 800);
         return false;
+    });
+
+    // show/hide mobile view
+    $(".nav-mobile-toggle").on('click', function () {
+        $(".nav_mobile").slideToggle();
     });
 
 });
