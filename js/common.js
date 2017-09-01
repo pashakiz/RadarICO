472715cdc179664d03f5e79edcc758e5f6227e98
pashakiz <pashakiz@gmail.com>
2017-09-01 23:57:27 +0300

    Page-3 fixed

--- js/common.js
+++ js/common.js
@@ -30,7 +30,10 @@ $(document).ready(function() {
 
     // .nav-filter expand
     $(".nav-filter__item").on('click', function () {
-        $(this).find('.dropdown').slideToggle();
+        $('.nav-filter__link').removeClass('active');
+        $(this).find('.nav-filter__link').addClass('active');
+        $('.nav-filter .dropdown').slideUp();
+        $(this).find('.dropdown').slideDown();
         $(this).find('.icon-arrow-down').toggleClass('up');
     });
 
